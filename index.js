d776be7fcd1170fb2b197925894e7cb3950a9190
Norman Tang <norman@normstorm.com>
2023-01-21 17:57:00 -0500

    basic slash commands

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // https://www.youtube.com/watch?v=qRMVNtIF73c
 
-const { Client, GatewayIntentBits } = require('discord.js')
+//const { Client, Events, GatewayIntentBits } = require('discord.js')
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
 require('dotenv/config')
 
 const client = new Client({
@@ -12,9 +15,17 @@ const client = new Client({
   ],
 })
 
-client.on('ready', () => {
-  console.log('The bot is ready')
-})
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
+
 
 client.on('messageCreate', (message) => {
   if (message.content === 'ping') {
@@ -23,5 +34,34 @@ client.on('messageCreate', (message) => {
     message.author.send("direct pong")
   }
 })
+/*
+client.on(Events.MessageCreate, (message) => {
+  if (message.content === 'ping') {
+    //message.reply('reply pong')
+    //message.channel.send("channel pong")
+    message.author.send("direct pong")
+  }
+});*/
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+  //console.log(interaction.commandName)
+	if (!command) {
+    return;
+  }
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
 
 client.login(process.env.DISCORD_TOKEN)
+
+client.on(Events.ClientReady, () => {
+  console.log('The bot is ready')
+})
