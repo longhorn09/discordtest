33ef3de8c811cab82290856d768a847ac7b5b263
Norman Tang <norman@normstorm.com>
2023-01-21 20:15:23 -0500

    built out basic slash commands basics

--- commands/stat.js
+++ commands/stat.js
@@ -1,12 +1,24 @@
 const { SlashCommandBuilder } = require('discord.js');
-
+/**
+ * ephemeral messages: https://discordjs.guide/slash-commands/response-methods.html#ephemeral-responses
+ * slash commands in bot DM: https://github.com/discord/discord-api-docs/issues/2365
+ * 
+ * 
+ */
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('stat')
-		.setDescription('Provides lore stats about object(s)'),
+		.setDescription('Provides lore stats about object(s)')
+		.addStringOption(option =>
+			option
+				.setName('item')
+				.setDescription('example: bronze.shield')
+				.setRequired(true)),
 	async execute(interaction) {
 		// interaction.user is the object representing the User who ran the command
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+		await interaction.reply({content: interaction.options.getString('item'), ephemeral: true});
+		//USER:   await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+		//SERVER: await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);		
 	},
 };
