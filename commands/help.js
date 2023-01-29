ed450809c2d46af966a946dc8f05b51d65b6b0d5
Norman Tang <norman@normstorm.com>
2023-01-28 23:35:23 -0500

    built out help.js

--- commands/help.js
+++ commands/help.js
@@ -5,8 +5,18 @@ module.exports = {
 		.setName('help')
 		.setDescription('Shows the list of Lorebot commands'),
 	async execute(interaction) {
-		// interaction.user is the object representing the User who ran the command
-		// interaction.member is the GuildMember object, which represents the user in the specific guild
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+		let helpMsg  = "** IRC Lore Bot v" + process.env.npm_package_version + ` **\n` + // (Items: ${numRows}) **\n` +
+			"!help    - Lists the different commands available\n" +
+			"!stat    - syntax: !stat <item>, example: !stat huma.shield\n" +
+			"!brief   - syntax: !brief <item>, example: !brief huma.shield\n" +
+			"!who     - shows character info, example: !who Drunoob\n" +
+			"!whoall  - shows all characters\n" +
+			//"!gton    - turn on output group chat\n" +
+			//"!gtoff   - turn off output to group chat\n" +
+			"!query   - flexible query with multiple crieria, example: !query affects=damroll by 2\n" +
+			"!recent  - shows latest lores and looks\n" +
+			"!version - shows version history\n";
+		await interaction.reply({ content: "```" + helpMsg + "```", epheremal: true		
+								});
 	},
 };
