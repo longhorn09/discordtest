36a10f9e6b344a7e0d4f84ff07daa7256b08b8e8
Norman Tang <norman@normstorm.com>
2023-01-28 23:34:40 -0500

    added functionality for help

--- index.js
+++ index.js
@@ -30,13 +30,82 @@ for (const file of commandFiles) {
 }
 
 
-
+// in discordjs version 13+, replace "message" with "messageCreate"
 client.on('messageCreate', (message) => {
+  let cmd = "";
+  let parsedCmd = ""
+
   if (message.content === 'ping') {
     message.reply('reply pong')
     message.channel.send("channel pong")
     message.author.send("direct pong")
+  
   }
+  
+  if (message.content.startsWith(process.env.DISCORD_PREFIX)) {
+    cmd = message.content.substring(1,message.content.length);
+    parsedCmd = cmd.split(" ")[0];
+
+    switch(parsedCmd) {
+      case "help":
+        //let helpStr = message.author.send("Use `/help` instead, `!help` is deprecated",{code:true});
+        //await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        //message.reply("Use `/help` instead, `!help` is deprecated", ephmeral: true)
+        message.reply({ content: "Use `/help` instead, `!help` is deprecated", ephmeral: true});
+        break;
+      default: 
+        break;
+    }
+  }
+  else if (message.content.trim().indexOf("Object '") >= 0   //need to do this way because lore might be pasted in middle of conversation
+        && message.author.username.substring(0,botname.length).toLowerCase() !== botname)
+  {
+    let loreArr = null, cleanArr = [];
+    //need to scrub the lore message for processing
+    loreArr = message.content.trim().split("Object '");
+    for (let i = 0 ; i < loreArr.length; i++)  {
+      if (loreArr[i].indexOf("'") > 0 && loreArr[i].indexOf(":"))
+      {
+        //console.log(`loreArr[${i}]: ${loreArr[i]}`);
+        cleanArr.push(`Object '${loreArr[i].trim()}`);
+      }
+    }
+    for (let i = 0 ;i < cleanArr.length;i++) {
+        //console.log(`cleanArr[${i}]: ${cleanArr[i]}`);
+        console.log("implement ParseLore")
+        //parseLore(message.author.username,cleanArr[i]);
+    }
+    loreArr = null;   //freeup for gc()
+    cleanArr = null;  //freeup for gc()
+  }
+  else if (message.content.trim().indexOf(" is using:") >0
+        && message.author.username.substring(0,botname.length).toLowerCase() !== botname)
+  {
+    let lookArr = null, cleanArr = [], charName = null;
+    lookArr = message.content.trim().split(/([A-Z][a-z]+) is using:/);
+    for (let i = 0; i < lookArr.length; i++) {
+      if  (/^([A-Z][a-z]+)$/.test(lookArr[i].trim())) {
+        charName = lookArr[i].trim();
+      }
+      else if (lookArr[i].trim().indexOf("<") === 0 && charName != null && charName.length > 0)
+      {
+        cleanArr.push(`${charName} is using:\n${lookArr[i].trim()}`);
+        charName = null;
+      }
+      else {
+        charName = null;
+      }
+    }
+    for (let i = 0; i < cleanArr.length; i++){
+      //console.log(`cleanArr[${i}]: ${cleanArr[i]}`);
+      console.log("implement ParseEqLook")
+      //ParseEqLook(message.author.username,cleanArr[i]);
+    }
+
+
+    cleanArr = null;
+    lookArr = null;
+  }   //end if  
 })
 /*
 client.on(Events.MessageCreate, (message) => {
@@ -64,8 +133,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
 	}
 });
 
-client.login(process.env.DISCORD_TOKEN)
-
+client.login(process.env.DISCORD_TOKEN)  // make sure the .env is setup
 client.on(Events.ClientReady, () => {
   console.log('The bot is ready')
 })
