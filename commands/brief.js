ffef900a961db39dccd72361a14d1ac186c8a197
Norman Tang <norman@normstorm.com>
2023-02-05 01:58:50 -0500

    build out brief.js

--- commands/brief.js
+++ commands/brief.js
@@ -17,7 +17,9 @@ module.exports = {
 	async execute(interaction) {
     let dateTime = ''
 		let replyMsg =''
-		let loreURL = "http://127.0.0.1:8080/api/v1/brief/" + interaction.options.getString('item') 
+		let loreURL = "http://" + process.env.DATABASE_IP + "/api/v1/brief/" + interaction.options.getString('item') 
+
+		console.log(loreURL)
 
 		await fetch(loreURL, {method: 'GET'}).then(
 			res => {
@@ -39,7 +41,7 @@ module.exports = {
             replyMsg = data.meta.loreCount + " items found for '" + interaction.options.getString('item') + "'\n"
           }
 				  for (let i = 0; i < data.data.length; i++) {
-					  replyMsg += data.data[i].OBJECT_NAME + "\n";
+					  replyMsg += "Object '" + data.data[i].OBJECT_NAME + "'\n";
 		  	  }
 	      }
         dateTime = moment().format("YYYY-MM-DD HH:mm:ss");
@@ -49,50 +51,3 @@ module.exports = {
 	},
 };
 
-/*
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('stat')
-		.setDescription('Provides lore stats about object(s)')
-	  .setDMPermission(true)
-		.addStringOption(option =>
-			option
-				.setName('item')
-				.setDescription('example: bronze.shield')
-				.setRequired(true)),
-	
-	async execute(interaction) {
-    let dateTime = ''
-		let replyMsg =''
-		let loreURL = "http://127.0.0.1:8080/api/v1/stat/" + interaction.options.getString('item') + "/1"
-
-		await fetch(loreURL, {method: 'GET'}).then(
-			res => {
-				if (res.ok) {  // note: response object contains entire HTTP response including headers
-					return res.json()
-				} else {
-					console.log("error")
-				}
-			})
-			.then(data => {
-				if (data.data.length === 0) {
-				  replyMsg = "0 item(s) found for '" + interaction.options.getString('item') + "'\n"	
-				}
-				else {
-          if ( data.meta.loreCount === 1) {
-            replyMsg = "1 item found for '" + interaction.options.getString('item') + "'\n"
-          }
-          else {
-            replyMsg = data.meta.loreCount + " items found for '" + interaction.options.getString('item') + "'\n"
-          }
-				  for (let i = 0; i < data.data.length; i++) {
-					  replyMsg += formatLore(data.data[i]);
-		  	  }
-	      }
-        dateTime = moment().format("YYYY-MM-DD HH:mm:ss");
-        console.log(`${dateTime} : ${interaction.user.username.toString().padEnd(30)} /stat ${interaction.options.getString('item')}`)
-        interaction.reply({ content: "```" + replyMsg + "```" , ephemeral: true});
-			})
-	},
-};
-*/
